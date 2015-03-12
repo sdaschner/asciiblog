89762e2ed35930bac0702cd54857a23131be141c
Sebastian Daschner <git@sebastian-daschner.de>
2015-03-12 14:08:11 +0100

    added license header

--- src/main/java/com/sebastian_daschner/asciiblog/business/source/boundary/SourceInvoker.java
+++ src/main/java/com/sebastian_daschner/asciiblog/business/source/boundary/SourceInvoker.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Sebastian Daschner, sebastian-daschner.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sebastian_daschner.asciiblog.business.source.boundary;
 
 import com.sebastian_daschner.asciiblog.business.entries.control.EntriesCache;
@@ -30,9 +46,7 @@ public class SourceInvoker {
     @PostConstruct
     @Schedule(second = "0", minute = "*", hour = "*")
     public void checkNewEntries() {
-        System.out.println("invoking entry check");
         final Map<String, String> changedFiles = gitExtractor.getChangedFiles();
-        System.out.println("changedFiles = " + changedFiles);
         changedFiles.entrySet().stream()
                 .map(e -> entryCompiler.compile(e.getKey(), e.getValue())).filter(Objects::nonNull)
                 .forEach(cache::store);
--- src/main/java/com/sebastian_daschner/asciiblog/business/source/control/GitExtractor.java
+++ src/main/java/com/sebastian_daschner/asciiblog/business/source/control/GitExtractor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Sebastian Daschner, sebastian-daschner.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sebastian_daschner.asciiblog.business.source.control;
 
 import com.sebastian_daschner.asciiblog.business.environment.control.Environment;
@@ -44,7 +60,6 @@ public class GitExtractor {
     @PostConstruct
     public void openGit() {
         try {
-            System.out.println("opening git repo");
             git = Git.cloneRepository().setDirectory(Files.createTempDirectory("blog-git").toFile()).setURI(gitDirectory.getAbsolutePath()).call();
         } catch (IOException | GitAPIException e) {
             throw new IllegalStateException("Could not open Git repository.", e);
@@ -182,7 +197,6 @@ public class GitExtractor {
 
     @PreDestroy
     public void closeGit() {
-        System.out.println("closing git repo");
         final File directory = git.getRepository().getWorkTree();
         git.close();
         delete(directory);
